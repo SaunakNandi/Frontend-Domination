fa176f146ef5dc00de80f75498e5104985f16523
Saunak Nandi <saunaknandi021000@gmail.com>
2024-03-12 06:37:08 +0530

    localstorge()

---
+++ Developer Problems/LocalStorage/script.js
@@ -0,0 +1,20 @@
+const block=document.querySelector('#block')
+const unblock=document.querySelector('#unblock')
+
+block.addEventListener('click',()=>{
+    localStorage.setItem('block',true)
+    show()
+})
+unblock.addEventListener('click',()=>{
+    localStorage.setItem('block',false)
+    show()
+})
+
+function show()
+{
+    if(localStorage.getItem('block')==='false')
+        document.querySelector('#status span').textContent='Not Blocked'
+    else
+        document.querySelector('#status span').textContent='Blocked'
+
+}
