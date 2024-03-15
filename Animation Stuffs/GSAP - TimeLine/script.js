d8766d2c023c49fe049150dbbdaf4141fc928281
Saunak Nandi <saunaknandi021000@gmail.com>
2024-03-15 06:36:55 +0530

    gsap timeline()

---
+++ Animation Stuffs/GSAP - TimeLine/script.js
@@ -0,0 +1,20 @@
+var tl=gsap.timeline()
+tl.to('#box1',{
+    x:1200,
+    rotate:360,
+    scale: 0.5,
+    duration:2,
+    delay:1,
+})
+tl.to('#box2',{
+    x:1200,
+    rotate:360,
+    scale: 0.5,
+    duration:2,
+})
+tl.to('#box3',{
+    x:1200,
+    rotate:360,
+    scale: 0.5,
+    duration:2,
+})
