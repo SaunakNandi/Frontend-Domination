09b9d641a900a5b345ea2116be8ed8579f7977be
Saunak Nandi <saunaknandi021000@gmail.com>
2024-03-15 06:25:23 +0530

    GSAP intro

---
+++ Animation Stuffs/GSAP/script.js
@@ -0,0 +1,15 @@
+// gsap.to('#box',{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor: 'rgba(255,255,255)'
+// })
+
+gsap.from("#box",{
+    x:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor: 'crimson'
+})
