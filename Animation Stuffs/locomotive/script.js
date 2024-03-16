9f1c27c82dd4f149c7438e447880e7fe74184053
Saunak Nandi <saunaknandi021000@gmail.com>
2024-03-16 21:58:43 +0530

    locomotive starting

--- Animation Stuffs/locomotive/script.js
+++ Animation Stuffs/locomotive/script.js
@@ -1,12 +1,5 @@
-gsap.to("#page2 h1",{
-    transform:"translateX(-125%)",
-    scrollTrigger:{
-        trigger:"#page2",
-        scroller:"body",
-        scrub:2,
-        markers:true,
-        start:"top 0",
-        end:"top -100%",
-        pin:true
-    }
-})
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+dispatchEvent.from("#page1")
