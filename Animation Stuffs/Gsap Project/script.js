43be20c60ee486a4e2ace34884e14b96ae38e5f5
Saunak Nandi <saunaknandi021000@gmail.com>
2024-03-17 11:47:52 +0530

    some changes

--- Animation Stuffs/Gsap Project/script.js
+++ Animation Stuffs/Gsap Project/script.js
@@ -1,23 +1,23 @@
-var tl=gsap.timeline()
-tl.from('#nav h3',{
-    y:-50,
-    opacity:0,
-    delay:0.4,
-    duration:1,
-    stagger:0.5
-})
+var tl = gsap.timeline();
+tl.from("#nav h3", {
+  y: -50,
+  opacity: 0,
+  delay: 0.4,
+  duration: 1,
+  stagger: 0.5,  // h1 will come 1 by 1
+});
 
-tl.from('#main h1',{
-    x:-500,
-    opacity:0,
-    duration:0.8,
-    stagger:0.5
-})
+tl.from("#main h1", {
+  x: -100,
+  opacity: 0,
+  duration: 0.8,
+  stagger: 0.5,
+});
 
-tl.from('img',{
-    x:100,
-    rotate:45,
-    opacity:0,
-    duration:0.5,
-    stagger:0.5
-})
+tl.from("img", {
+  x: 100,
+  rotate: 45,
+  opacity: 1,
+  duration: 1,
+  stagger: 0.5,
+});
