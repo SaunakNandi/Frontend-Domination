71fea455398559a43cee2a1565c4a41cf1f692c8
SaunakNandi <saunaknandi021000@gmail.com>
2024-03-26 23:39:47 +0530

    bug in glooy effect

--- Animated Websites Projects/Obys Clone/script.js
+++ Animated Websites Projects/Obys Clone/script.js
@@ -61,7 +61,6 @@ function loadingAnimation() {
     opacity: 0,
     delay: 2,
     duration: 0.2,
-    display: "none",
   });
   tl.from("#page1", {
     y: 1600,
@@ -69,6 +68,9 @@ function loadingAnimation() {
     opacity: 0,
     duration: 0.5,
     ease: Power4,
+  });
+  tl.to("#loader", {
+    display: "none",
   });
   tl.from('#nav',{
     opacity: 0,
@@ -89,23 +91,70 @@ function crsrAnimation() {
       top: e.y,
     });
   });
-  Shery.makeMagnet("#nav-part2 h4" /* Element to target.*/, {
-    //Parameters are optional.
-    // ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    // duration: 1,
-  });
-}
+  Shery.makeMagnet("#nav-part2 h4");
+  var videoContainer=document.querySelector("#video-container")
+  var video=document.querySelector("#video-container video")
+  var crsr=document.querySelector("#crsr")
+  videoContainer.addEventListener('mouseenter',()=>{
+    gsap.to("#crsr",{
+      display:"none"
+    })
+    crsr.style.display="none"
+    videoContainer.addEventListener('mousemove',(dets)=>{
+      gsap.to("#video-cursor",{
+        left:dets.x-400,
+        y:dets.y-200
+      })
+    })
 
-locomotiveAnimation()
-loadingAnimation();
-crsrAnimation();
+    let flag=0
+    videoContainer.addEventListener("click",()=>{
+      if(flag===0)
+      {
+        video.play()
+        video.style.opacity=1
+        document.querySelector("#video-cursor").innerHTML=`<i class="ri-pause-line"></i>`
+        gsap.to("#video-cursor",{
+          scale:0.5
+        })
+        flag=1
+      }
+      else{
+        video.pause()
+        video.style.opacity=0
+        document.querySelector("#video-cursor").innerHTML=`<i class="ri-play-mini-fill"></i>`
+        gsap.to("#video-cursor",{
+          scale:1
+        })
+        // videoContainer.style.backgroundImage=`https://obys.agency/wp-content/uploads/2022/11/Showreel-2022-preview-1.jpg`
+        flag=0
+      }
+    })
+    
+  })
+  videoContainer.addEventListener('mouseleave',()=>{
+    gsap.to("#crsr",{
+      border:"1px solid #ffffff7d",
+      display:"initial"
+    })
+    gsap.to("#video-cursor",{
+      top:"-15%",
+      left:"70%"
+    })
+  })
+
+}
 
 function sheryAnimation(){
   Shery.imageEffect(".image-div",{
     style:5,
-    debug:true,
-    gooey:true,
+    // debug:true,
+    // gooey:true,
     config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.6969697648390822},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.76,"range":[0,10]},"metaball":{"value":0.49,"range":[0,2]},"discard_threshold":{"value":0.54,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
   })
 }
+
+locomotiveAnimation()
+loadingAnimation();
 sheryAnimation()
+crsrAnimation();
