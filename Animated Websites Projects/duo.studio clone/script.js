a3186e36020a6b80e9a469ed2877ae674a640331
Saunak Nandi <saunaknandi021000@gmail.com>
2024-04-01 01:45:58 +0530

    duo.studio just started

---
+++ Animated Websites Projects/duo.studio clone/script.js
@@ -0,0 +1,37 @@
+function loco() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+}
+loco()
+
+gsap.to(".page1 h1",{
+    x:-100,
+    duration:1,
+    scrollTrigger:{
+        trigger:".page1 h1",
+        scroller:"#main",
+        markers:true,
+        start:"top 30%",
+        end:"top 0",
+    }
+})
