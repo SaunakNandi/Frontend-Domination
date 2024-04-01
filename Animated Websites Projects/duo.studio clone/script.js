ae3bb8b30f3ec77a6e0daf05bd49a3edc19727c8
Saunak Nandi <saunaknandi021000@gmail.com>
2024-04-01 10:48:02 +0530

    page4 started

--- Animated Websites Projects/duo.studio clone/script.js
+++ Animated Websites Projects/duo.studio clone/script.js
@@ -1,37 +1,104 @@
 function loco() {
-    gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger);
 
-    const locoScroll = new LocomotiveScroll({
-        el: document.querySelector("#main"),
-        smooth: true
-    });
-    locoScroll.on("scroll", ScrollTrigger.update);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-    ScrollTrigger.scrollerProxy("#main", {
-        scrollTop(value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-        getBoundingClientRect() {
-            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-        },
-        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-    });
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
 
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-    ScrollTrigger.refresh();
+  ScrollTrigger.refresh();
 }
-loco()
+loco();
 
-gsap.to(".page1 h1",{
-    x:-100,
-    duration:1,
-    scrollTrigger:{
-        trigger:".page1 h1",
-        scroller:"#main",
-        markers:true,
-        start:"top 30%",
-        end:"top 0",
-    }
-})
+var crsr = document.querySelector("#crsr");
+var main = document.querySelector("#main");
+main.addEventListener("mousemove", (coord) => {
+  crsr.style.left = coord.x + "px";
+  crsr.style.top = coord.y + "px";
+});
+
+gsap.from(".page1 h1,.page1 h2", {
+  y: 70,
+  rotate: 4,
+  opacity: 0,
+  delay: 0.3,
+  duration: 0.8,
+});
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".page1 h1",
+    scroller: "#main",
+    // markers: true,
+    start: "top 25%",
+    end: "top 0",
+    scrub: 2,
+  },
+});
+tl.to(
+  ".page1 h1",
+  {
+    x: -80,
+  },
+  "flag"
+)
+  .to(
+    ".page1 h2",
+    {
+      x: 100,
+    },
+    "flag"
+  )
+  .to(
+    ".page1 video",
+    {
+      width: "90%",
+    },
+    "flag"
+  );
+
+var tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".page1 h1",
+    scroller: "#main",
+    // markers: true,
+    start: "top -110%",
+    end: "top -120%",
+    scrub: 1,
+  },
+});
+tl2.to(
+  "#main",
+  {
+    backgroundColor: "#fff",
+  },
+  "change"
+);
+tl2.from(
+  "video",
+  {
+    width: "90%",
+  },
+  "change"
+);
