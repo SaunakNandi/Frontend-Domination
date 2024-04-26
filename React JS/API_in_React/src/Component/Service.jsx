909fff43c521b5f9744c5b4f69da7a74eaed43ab
Saunak Nandi <saunaknandi021000@gmail.com>
2024-04-26 17:21:16 +0530

    Learned useEffect and store api in one file and import the api from there.

--- React JS/API_in_React/src/Component/Service.jsx
+++ React JS/API_in_React/src/Component/Service.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import { useEffect,useState } from 'react';
+import axios from "../utils/axios"
 
 const Service = () => {
-    useEffect(() =>{
-        console.log('Service comp created')
-        return ()=>{
-          console.log('Service comp destroyed')
-        }
-      })
-      return (
-        <div>Service</div>
-      )
-}
+    const [first,setFirst]=useState("this is normal data")
+    const [second,setSecond]=useState("this is very large data")
 
+    const getUsers = () =>{
+      axios.get("/users").then(users => {
+        console.log(users)
+      }).catch(err=> console.log(err))
+    }
+    useEffect(()=>{
+      console.log("Service component is created")
+
+      return ()=>{
+        console.log("Service component is destroyed")
+      }
+    },[second])
+    return(
+      <div>
+        <h1>{first}</h1>
+        <button onClick={()=>{setFirst("Normal data has been changed")}} 
+        className="rounded mb-10 bg-red-200 p-2">Change Normal Data</button>
+        <h1>{second}</h1>
+        <button onClick={()=>{setSecond("Large data has been changed")}} 
+        className="rounded mb-10 bg-red-200 p-2">Change large Data</button>
+      </div>
+    )
+}
 export default Service;
--- React JS/API_in_React/src/Component/Show.jsx
+++ React JS/API_in_React/src/Component/Show.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { useState } from 'react';
-import axios from 'axios';
+import { useState,useEffect } from 'react';
+import axios from '../utils/axios';
 const Show = () => {
     const [products,setProducts] =useState([])
     const getProducts = () =>{
-      const api="https://fakestoreapi.com/products"
-      axios.get(api).then(products => {
+      axios.get("/products").then(products => {
         console.log(products)
         setProducts(products.data)
       }).catch(err=> console.log(err))
     }
     const addProducts = () =>{
-        const api="https://fakestoreapi.com/products"
-        axios.post(api,{
+        axios.post("/products",{
           title:"test product",
           price:13.5,
           description: "lorem ipsum",
@@ -22,15 +20,19 @@ const Show = () => {
           console.log(res)
         }).catch(err=> console.log(err))
       }
+    
+    useEffect(() => { 
+      getProducts() 
+    }, []);
   return (
     <>
-        <button onClick={getProducts} className='px-2 py-5 bg-red-300 rounded-md'>Call Products</button>
+        {/* <button onClick={getProducts} className='px-2 py-5 bg-red-300 rounded-md'>Call Products</button>
         <br />
-        <br />
+        <br /> */}
         <button onClick={addProducts} className='px-2 py-5 bg-red-300 rounded-md'>Save Item</button>
         <hr className='my-10'/>
-        <ul>
-          {products.length>0? products.map((x,index)=><li key={index} className='rounded w-1/4 p-5 mt-3 bg-red-100'>${x.title}</li>):'Loading...'}
+        <ul className="pt-5">
+          {products.length>0? products.map((x,index)=><li key={index} className='rounded w-1/4 p-5 mt-3 bg-red-100'>{x.title}</li>):'Loading...'}
         </ul>
     </>
   )
