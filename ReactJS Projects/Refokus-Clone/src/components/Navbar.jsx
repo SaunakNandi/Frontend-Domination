4b047eedb382a47cb544457e086cded0409207dc
Saunak Nandi <saunaknandi021000@gmail.com>
2024-05-02 19:11:00 +0530

    New Project Refokus

---
+++ ReactJS Projects/Refokus-Clone/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='max-w-screen-xl mx-auto bg-zinc-800 py-6'>
+            
+        </div>
+    );
+}
+
+export default Navbar;
