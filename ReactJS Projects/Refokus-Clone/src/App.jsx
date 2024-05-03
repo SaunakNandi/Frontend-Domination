1813ac60e1b3cc0f1f9a8f2d1cf8c2fe9dc14d0a
Saunak Nandi <saunaknandi021000@gmail.com>
2024-05-03 08:24:36 +0530

    navbar almost done. Some images are added.

--- ReactJS Projects/Refokus-Clone/src/App.jsx
+++ ReactJS Projects/Refokus-Clone/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="w-full h-screen bg-zinc-900 text-white">
-        
+      <div className='w-full h-screen bg-zinc-900 text-white font-["satoshi_variable"]'>
+        <Navbar />  
       </div>
     </>
   )
--- ReactJS Projects/Refokus-Clone/src/components/Navbar.jsx
+++ ReactJS Projects/Refokus-Clone/src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from 'react'
+import refokus_logo from '../assets/refokus_logo.svg'
+
 
 const Navbar = () => {
     return (
-        <div className='max-w-screen-xl mx-auto bg-zinc-800 py-6'>
-            
+        <div className='max-w-screen-xl mx-auto py-6 flex items-center'>
+            <img src={refokus_logo} alt="" />
+            <div className="links flex gap-14 ml-20">
+                {["Home","Work","Culture","","News"].map((elem,index)=>(
+                    
+                    elem.length===0? (<span className='w-1 h-10 bg-zinc-800'></span>):
+                    (
+                        <a href="#" className="font-regular text-sm flex items-center gap-1">
+                        {index===0 && (<span style={{boxShadow:"0 0 0.25em #00FF19"}}
+                         className="inline-block w-1 h-1 bg-green-500 rounded-full"></span>)}
+                        {elem}
+                        </a>
+                    )
+                ))}
+            </div>
         </div>
     );
 }
