2b390ac1972a0cb6df7fef2c14b7e8b4a2a4a3af
Saunak Nandi <saunaknandi021000@gmail.com>
2024-05-05 19:40:47 +0530

    form error fixed

--- React JS/Form-Handling/src/App.jsx
+++ React JS/Form-Handling/src/App.jsx
@@ -1,21 +1,23 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 
 import './App.css'
 
 function App() {
   
-  const name=useRef(null)
-  const age=useRef(null)
+  const [val,setVal]=useState({name:"",age:""})
 
   const handleSubmit=(event)=>{
     event.preventDefault()
-    console.log(name.currentValue)
+    console.log(val)
   }
   return (
     <>
       <form action="" onSubmit={handleSubmit} className='flex gap-3'>
-        <input className='border-2' type="text" ref={name} placeholder="name" />
-        <input className='border-2' type="age" ref={age} placeholder="age" />
+        <input className='border-2' type="text" 
+        onChange={(event)=>setVal({...val,name:event.target.value})}
+        placeholder="name" />
+        <input className='border-2' type="age" placeholder="age" 
+        onChange={(event)=>setVal({...val,age:event.target.value})}/>
         <input className='border-2' type="submit"/>
       </form>
     </>
