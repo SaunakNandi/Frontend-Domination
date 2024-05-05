6e86f2c2f4f75158f14268a02f48fc718232a879
Saunak Nandi <saunaknandi021000@gmail.com>
2024-05-05 12:34:30 +0530

    Marquee done

--- Projects/Refokus-Clone/src/components/Marquee.jsx
+++ Projects/Refokus-Clone/src/components/Marquee.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { motion } from 'framer-motion'
 
-const Marquee = ({imagesurls}) => {
+const Marquee = ({imagesurls,direction}) => {
   return (
     // can you add whitespace-nowrap
-    <div className='flex w-full py-5 gap-20 overflow-hidden'>
-        {imagesurls.map((url,index)=><img key={index} src={url} className='w-28 flex-shrink-0'/>)}
-        {imagesurls.map((url,index)=><img key={index} src={url} className='flex-shrink-0'/>)}
+    <div className='flex w-full overflow-hidden'>
+      <motion.div  className="flex flex-shrink-0 gap-40 py-10 pr-40"
+        initial={{x: (direction==='left')? "0": "-100%"}} 
+        animate={{x: (direction==='left')? "-100%": "0"}} 
+        transition={{ease:"linear", duration:15, repeat:Infinity}}>
+          {imagesurls.map((url,index)=>(<img key={index} src={url} className=''/>))}
+      </motion.div>
+      <motion.div className="flex flex-shrink-0 gap-40 py-10 pr-40"
+        initial={{x: (direction==='left')? "0": "-100%"}} 
+        animate={{x: (direction==='left')? "-100%": "0"}} 
+        transition={{ease:"linear", duration:15, repeat:Infinity}}>
+          {imagesurls.map((url,index)=>(<img key={index} src={url} className=''/>))}
+      </motion.div>
     </div>
   )
 }
