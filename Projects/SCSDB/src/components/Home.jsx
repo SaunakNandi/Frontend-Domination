55d8b56396a8b6de4b7ab0870811dc8158553ffa
Saunak Nandi <saunaknandi021000@gmail.com>
2024-05-07 19:22:24 +0530

    started

---
+++ Projects/SCSDB/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sidenav from './template/Sidenav';
+
+
+const Home = () => {
+    document.title='Movie App'
+    return (
+        <>
+            <Sidenav/>
+            <div className='w-[80%] h-full'>
+                
+            </div>
+        </>
+    );
+}
+
+export default Home;
