9f4812a6d844cf88c2cb4f7fe78600796aaac418
Saunak Nandi <saunaknandi021000@gmail.com>
2024-05-18 10:30:22 +0530

    nav link for movie details

--- Projects/SCSDB/src/components/MovieDetails.jsx
+++ Projects/SCSDB/src/components/MovieDetails.jsx
@@ -22,12 +22,12 @@ const MovieDetails = () => {
     <div className='w-screen h-screen px-[10%]'
     style={{background:`linear-gradient(rgba(0,0,0,.2),rgba(0,0,0,.3),rgba(0,0,0,.8)),url(https://image.tmdb.org/t/p/original/${info.detail.backdrop_path || info.detail.poster_path})`,
     backgroundPosition: `top`,backgroundSize: `cover`,backgroundRepeat:'no-repeat'}}>
-        <nav className='w-full text-zinc-400'>
+        <nav className='h-[10vh] w-full text-zinc-400 flex gap-10 items-center'>
           {/* back icon */}
           <Link onClick={()=>navigate(-1)} 
-          className="mr-2 ri-arrow-left-line hover:text-[#6556CD] cu"></Link>  
+          className="mr-2 ri-arrow-left-line hover:text-[#6556CD] font-2xl"></Link>  
           {/* using <a> tag because we need to redirect it to different page*/}
-          <a href="">
+          <a href={`info.detail.homepage`} target='_blank'>
             <i className="ri-external-link-fill"></i>
           </a>
           <a href="">
