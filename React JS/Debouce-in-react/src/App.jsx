016abf34e64b54a32e7d5e8a0de29013d48aed5a
Saunak Nandi <saunaknandi021000@gmail.com>
2024-05-19 23:07:11 +0530

    debouce in react

---
+++ React JS/Debouce-in-react/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useCallback } from 'react';
+import debounce from 'lodash.debounce';
+import './App.css'
+function App() {
+  const data = ['John', 'Jane', 'Joe', 'Jill', 'Jake'];
+
+  const getSearchResults = async (searchTerm) => {
+    console.log('Making API request');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return data.filter((res) => res.toLowerCase().includes(searchTerm.toLowerCase()));
+  };
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  // if useCallback is not used then -> 
+  // debouncedRequest is created anew on every render. Therefore, the debounce logic does not work correctly because each new function does not have the internal state of the previous one.
+  // Using useCallback ensures that the debouncedRequest function is memoized and only recreated when its dependencies change (in this case, there are no dependencies, so it is created only once).
+  const debouncedRequest = useCallback(
+    debounce(async (searchTerm) => {
+      const results = await getSearchResults(searchTerm);
+      setSearchResults(results);
+    }, 500),
+    []
+  );
+
+  return (
+    <>
+      <input type="text" onChange={(e)=>debouncedRequest(e.target.value)} />
+      <div className="flex flex-col">
+        Results:
+        {searchResults.map((res, i) => (
+          <div key={i}>{res}</div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
